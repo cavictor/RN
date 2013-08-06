43fe0c06f5821ed7d6ef033bd1a9c8e373e0dcf8
cavictor <cvictor@bblanca.com.ar>
2013-08-06 13:58:02 -0300

    primer prueba
    para ver si funciona

---
+++ M1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="sp">
+
+	<head>
+		<meta charset="utf-8" />
+		<meta name="application-name" content="RN" />
+		<meta name="author" content="CV" />
+		<meta name="description" content="Sitio MOVIL" />
+		<meta name="designer" content="CV" />
+		<meta name="generator" content="manual" />
+		<meta name="googlebot" content="all, index, follow, noarchive, noimageindex" />
+		<meta name="keywords" content="RN" />
+		<meta name="publisher" content="notepad solamente" />
+		<meta name="rating" content="general" />
+		<meta name="revisit-after" content="2 days" />
+		<meta name="robots" content="all, index, follow, noarchive, noimageindex" />
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
+		 
+	<title>RN</title>
+	<!-- Hoja/s de estilo y archivos JavaScript -->
+
+
+
+	</head>
+
+
+
+<body>
+<h3>Begin</h3>
+<div style="width: 250px; background-color:light-pink";>
+<div style="width: 200px; white-space: wrap; border: 1px solid red; overflow: hidden; text-overflow: ellipsis;">
+<span style="font-size:13pt; text-shadow: 3px 3px blue;">Hola mundo, aqui estoy !.</span>
+</span>
+</div><BR>
+</div>
+
+</body>
